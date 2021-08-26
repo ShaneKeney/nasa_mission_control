b249f950de9f79eaac89c103bee07ba669945b73
Shane Keney <shane@spikeball.com>
2021-08-26 07:36:44 -0400

    feat: wired up csv reader feat: wired up getAllPlanets

--- server/src/controllers/planets.js
+++ server/src/controllers/planets.js
@@ -1,7 +1,6 @@
-const planets = require('../models/planets')
+const { planets } = require('../models/planets')
 
 function getAllPlanets(req, res) {
-  console.log('here')
   return res.status(200).json(planets)
 }
 
--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,18 @@
 const http = require('http')
 const app = require('./app')
+const { loadPlanets } = require('./models/planets')
 
 const PORT = process.env.PORT || 8000
 
 const server = http.createServer(app)
 
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`)
-})
+async function startServer() {
+  await loadPlanets()
+
+  server.listen(PORT, async () => {
+    console.log(`Listening on port ${PORT}...`)
+  })  
+}
+
+startServer()
+
